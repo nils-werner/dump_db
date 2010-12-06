5e27a5a59e7d76c5a7bf040b48ea55a1975b0c73
Nils Werner <nils.werner@gmail.com>
2010-12-06 16:34:39 +0100

    Adjusted german translation

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,7 +7,7 @@
 			'email' => 'nils.werner@gmail.com',
 			'website' => 'http://www.phoque.de'
 		),
-		'release-date' => '2010-10-26'
+		'release-date' => '2010-12-06'
 	);
 	
 	
@@ -17,41 +17,44 @@
 	 */
 
 	$dictionary = array(
-
-		'Dump Database' =>
-		'Datenbank sichern',
-
-		'Dump' =>
-		'Sichern',
+	
+		'Authors' =>
+		'Autoren',
 		
-		'Restore' =>
-		'Wiederherstellen',
+		'Data' => 
+		'Daten',
 
-		'Packages and restores your database into and from <code>%s/%s</code>.' =>
-		'Speichert und stellt die Datenbank wieder her in/aus <code>%s/%s</code>.',
+		'Save Data' =>
+		'Daten sichern',
+
+		'Save Authors' =>
+		'Autoren sichern',
+		
+		'Restore Data' =>
+		'Daten wiederherstellen',
+
+		'Restore Authors' =>
+		'Autoren wiederherstellen',
+
+		'Packages and restores your data and authors into and from <code>%s/%s</code>.' =>
+		'Speichert und stellt ihre Daten und Autoren wieder her in/aus <code>%s/%s</code>.',
 		
 		
 		'An error occurred while trying to write <code>%s/%s</code>.' =>
 		'Ein Fehler ist beim Schreiben von <code>%s/%s</code> aufgetreten.',
 		
 		
-		'Database successfully dumped into <code>%s/%s</code>.' =>
-		'Datenbank wurde erfolgreich in <code>%s/%s</code> gespeichert.',
+		'%s successfully dumped into <code>%s/%s</code>.' =>
+		'%s wurde erfolgreich in <code>%s/%s</code> gespeichert.',
 		
 		'random-hash' =>
 		'zufälliger-hash',
 		
-		'Save author information' =>
-		'Benutzer sichern',
-		
-		'After checking this box, all authordata will be overwritten if the dump is being restored.' =>
-		'Wenn dieses Häkchen aktiviert wird, werden beim Wiedereinspielen der Sicherung sämtliche Benutzerdaten überschrieben',
-		
 		'Do you really want to overwrite your database with the contents from the file?' =>
 		'Wollen Sie wirklich die Datenbank mit dem Inhalt der Datei überschreiben?',
 		
-		'Database successfully restored from <code>%s/%s</code> in %d queries.' =>
-		'Datenbank erfolgreich aus <code>%s/%s</code> in %d Abfragen wiederhergestellt.',
+		'%s successfully restored from <code>%s/%s</code> in %d queries.' =>
+		'%s erfolgreich aus <code>%s/%s</code> in %d Abfragen wiederhergestellt.',
 		
 		'An error occurred while trying to import from <code>%s/%s</code>.' =>
 		'Ein Fehler ist bei der Wiederherstellung der Datenbank aus <code>%s/%s</code> aufgetreten.',
