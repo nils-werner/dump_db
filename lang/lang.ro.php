2a7caeca381b132b45536f7601a09e8e2356b6e6
Nils Werner <nils.werner@gmail.com>
2011-02-08 23:09:11 +0100

    Updated author info in romanian translation.
    Conflicts:
    
    	lang/lang.ro.php

---
+++ lang/lang.ro.php
@@ -0,0 +1,55 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad?micutul@yahoo.com',
+		),
+		'release-date' => '2011-02-08'
+	);
+
+	/**
+	 * Dump DB
+	 */
+	$dictionary = array(
+
+		// Missing
+
+		'random-hash' => 
+		'hash aleator',
+
+		'Dump Database' => 
+		'Dump la Baza de Date',
+
+		'Save Authors' => 
+		'Salvează Autorii',
+
+		'Save Data' => 
+		'Salvează Datele',
+
+		'Restore Authors' => 
+		'Restabileşte Autorii',
+
+		'Restore Data' => 
+		'Restabileşte Datele',
+
+		'Packages and restores your data and authors into and from <code>%s/%s</code>.' => 
+		'Salvează şi restabileşte datele şi autorii în şi din <code>%s/%s</code>.',
+
+		'%s successfully restored from <code>%s/%s</code> in %d queries.' => 
+		'%s restabilit cu succes din <code>%s/%s</code> în %d interogări.',
+
+		'An error occurred while trying to import from <code>%s/%s</code>.' => 
+		'A intervenit o eroare la importarea din <code>%s/%s</code>.',
+
+		'%s successfully dumped into <code>%s/%s</code>.' => 
+		'%s salvat cu succes în <code>%s/%s</code>.',
+
+		'An error occurred while trying to write <code>%s/%s</code>.' => 
+		'A intervenit o eroare la scrierea <code>%s/%s</code>.',
+
+		'Do you really want to overwrite your database with the contents from the file?' => 
+		'Sunteţi sigur că doriţi să suprascrieţi baza de date cu conţinutul acestui fişier?',
+
+	);
