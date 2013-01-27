100d472087fd25cdd04830fb75b4be42a75fad11
Davide Grobberio <dadeg90@hotmail.it>
2013-01-27 09:30:49 -0800

    Create lang.it.php

---
+++ lang/lang.it.php
@@ -0,0 +1,56 @@
+<?php
+
+  $about = array(
+		'name' => 'Italian',
+		'author' => array(
+			'name' => 'Davide Grobberio',
+			'email' => 'davide@zaniniadv.it',
+			'website' => 'http://www.zaniniadv.it'
+		),
+		'release-date' => '2013-01-27'
+	);
+
+	/**
+	 * Dump DB
+	 */
+	$dictionary = array(
+
+		// Missing
+
+		'random-hash' => 
+		'hash casuale',
+
+		'Dump Database' => 
+		'Dump database',
+
+		'Save Authors' => 
+		'Salva autori',
+
+		'Save Data' => 
+		'Salva dati',
+
+		'Restore Authors' => 
+		'Reimposta autori',
+
+		'Restore Data' => 
+		'Reimposta dati',
+
+		'Packages and restores your data and authors into and from <code>%s/%s</code>.' => 
+		'Impacchetta e ripristina dati e autori da/a <code>%s/%s</code>.',
+
+		'%s successfully restored from <code>%s/%s</code> in %d queries.' => 
+		'%s ripristinato con successo da <code>%s/%s</code> în %d interrogazioni.',
+
+		'An error occurred while trying to import from <code>%s/%s</code>.' => 
+		'Si è verificato un errore importando da <code>%s/%s</code>.',
+
+		'%s successfully dumped into <code>%s/%s</code>.' => 
+		'%s dump eseguito con succeso in <code>%s/%s</code>.',
+
+		'An error occurred while trying to write <code>%s/%s</code>.' => 
+		'Si è verificato un errore provando a scrivere <code>%s/%s</code>.',
+
+		'Do you really want to overwrite your database with the contents from the file?' => 
+		'Sicuro di voler sovrascrivere il tuo database con altri contenuti?',
+
+	);
